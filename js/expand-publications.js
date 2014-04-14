718e5d2390b5bb42f00da9f9fe369eaa5e6f981b
Zack Elliott <hubridnoxx@gmail.com>
2014-04-14 15:31:22 -0400

    cleaned up css and added variable names

--- js/expand-publications.js
+++ js/expand-publications.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
     $("._project-pub-link").click(function() {
         var dropdown = $(this).next(".dropdown-pub-links"),
-            arrow = $(this).children(".fa-angle-down");
+            arrow = $(this).children(".fa-angle-down"),
+            label = $(this).children("span");
         if(dropdown.hasClass("open")) {
+            label.text("Expand publications");
             $.rotate(0, arrow);
-            $(this).html("<i class='fa fa-file-text-o'></i>Expand publications<i class='fa fa-angle-down'></i>");
             dropdown.removeClass("open").animate({
                 height: "0px",
                 padding: "0 10px 0 10px"
             }, 200, function() {
             });
         } else {
+            label.text("Hide publications");
             $.rotate(180, arrow);
-            $(this).html("<i class='fa fa-file-text-o'></i>Hide publications<i class='fa fa-angle-down'></i>");
             dropdown.addClass("open").animate({
                 height: "314px",
                 padding: "10px"
