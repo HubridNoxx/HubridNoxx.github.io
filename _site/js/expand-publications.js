17ed6032c16bf52a06ef20cd79fcd257fddb88d8
Zack Elliott <hubridnoxx@gmail.com>
2014-04-20 12:00:57 -0400

    changed expand publications again

--- _site/js/expand-publications.js
+++ _site/js/expand-publications.js
@@ -1,24 +1,39 @@
 $(document).ready(function() {
     $("._project-pub-link").click(function() {
         var dropdown = $(this).next(".dropdown-pub-links"),
+            dropdown_span = dropdown.children("span"),
+            dropdown_div = dropdown.children("div"),
             arrow = $(this).children(".fa-angle-down"),
             label = $(this).children("span");
         if(dropdown.hasClass("open")) {
             label.text("Expand publications");
             $.rotate(0, arrow);
             dropdown.removeClass("open").animate({
-                height: "0px",
-                padding: "0 10px 0 10px"
+                height: "0px"
+            }, 200, function() {
+            });
+            dropdown_span.animate({
+                padding: "0px 10px"
+            }, 200, function() {
+            });
+            dropdown_div.animate({
+                padding: "0px 10px"
             }, 200, function() {
             });
         } else {
             label.text("Hide publications");
             $.rotate(180, arrow);
             dropdown.addClass("open").animate({
-                height: "314px",
+                height: "314px"
+            }, 200, function() {
+            });
+            dropdown_span.animate({
                 padding: "10px"
             }, 200, function() {
-            // Animation complete.
+            });
+            dropdown_div.animate({
+                padding: "10px"
+            }, 200, function() {
             });
         }
     });
